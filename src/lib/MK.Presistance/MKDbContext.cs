8203c06eaf6afb78f4cd4bbac740df11adc8b22a
mohamed korain <lazzez22@gmail.com>
2021-05-28 18:09:38 +0200

    add mk db context

---
+++ src/lib/MK.Presistance/MKDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MK.Presistance
+{
+    /// <summary>
+    /// Represent Entities Store in Database
+    /// </summary>
+    public sealed class MKDbContext : DbContext
+    {
+        #region ctor
+
+        public MKDbContext(DbContextOptions<MKDbContext> options) : base(options)
+
+        {
+        }
+
+        #endregion ctor
+
+        #region Methods
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MKDbContext).Assembly);
+        }
+
+        #endregion Methods
+    }
+}
