2e7cb2f583985390e39bb80bd5053ee6c10ad92f
mohamed korain <lazzez22@gmail.com>
2021-05-28 17:11:04 +0200

    Add IRepository Interface With Basic functions [insert,update,delete,query] for table

---
+++ src/lib/MK.Presistance/IRepository.cs
@@ -0,0 +1,80 @@
+﻿using MK.Domain;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MK.Presistance
+{
+    /// <summary>
+    /// represent data access using repository pattern
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        /// <summary>
+        /// Insert entity to data
+        /// </summary>
+        /// <param name="entity">entity to insert</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the  entity entry after insert
+        /// </returns>
+        Task<TEntity> InsertAsync(TEntity entity);
+
+        /// <summary>
+        /// Insert entity to data
+        /// </summary>
+        /// <param name="entity">entity to insert</param>
+        /// <returns>inserted entity</returns>
+        TEntity Insert(TEntity entity);
+
+        /// <summary>
+        /// Delete entity from data
+        /// </summary>
+        /// <param name="id">entity identifier</param>
+        /// <returns> A task that represents the asynchronous operation</returns>
+        Task DeleteAsync(int id);
+
+        /// <summary>
+        /// Delete entity from data
+        /// </summary>
+        /// <param name="id">entity identifier</param>
+        /// <returns></returns>
+        void Delete(int id);
+
+        /// <summary>
+        /// Delete entity from data
+        /// </summary>
+        /// <param name="entity">entity to delete</param>
+        /// <returns> A task that represents the asynchronous operation</returns>
+        Task DeleteAsync(TEntity entity);
+
+        /// <summary>
+        /// Delete entity from data
+        /// </summary>
+        /// <param name="entity">entity to delete</param>
+        /// <returns></returns>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// update entity in data
+        /// </summary>
+        /// <param name="entity">entity to update</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the  entity entry after update</returns>
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        /// <summary>
+        /// update entity in data
+        /// </summary>
+        /// <param name="entity">entity to update</param>
+        /// <returns>
+        /// entity entry after update</returns>
+        Task<TEntity> Update(TEntity entity);
+
+        /// <summary>
+        ///  Gets a table
+        /// </summary>
+        IQueryable Table { get; }
+    }
+}
