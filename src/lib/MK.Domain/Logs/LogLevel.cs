039041a3ef355ce2c4053919130400de4e5393f7
mohamed korain <lazzez22@gmail.com>
2021-05-28 18:24:20 +0200

    add log entity

---
+++ src/lib/MK.Domain/Logs/LogLevel.cs
@@ -0,0 +1,32 @@
+﻿namespace MK.Domain.Logs
+{
+    /// <summary>
+    /// log level type
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// error level
+        /// usage : when error happen and will affect programe logic
+        /// </summary>
+        Error = 1,
+
+        /// <summary>
+        /// information level
+        /// usage: when developer want to log information for trace
+        /// </summary>
+        Information = 2,
+
+        /// <summary>
+        /// critical error level
+        /// usage: when error happen and program will be down
+        /// </summary>
+        Critical = 3,
+
+        /// <summary>
+        /// warning error level
+        /// usage : when error happend and will not affect the programe logic
+        /// </summary>
+        Warning = 4,
+    }
+}
---
+++ src/lib/MK.Domain/Logs/SystemLogs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MK.Domain.Logs
+{
+    /// <summary>
+    /// Represent system log entity
+    /// </summary>
+    public class SystemLogs : BaseEntity
+    {
+        public string Description { get; set; } = null;
+        public string FullException { get; set; } = null;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
