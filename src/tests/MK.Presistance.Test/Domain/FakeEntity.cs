eaef3140b48ed618dee06060076e91f91fced09c
mohamed korain <lazzez22@gmail.com>
2021-05-28 17:21:27 +0200

    add fake entity for testing

---
+++ src/tests/MK.Presistance.Test/Domain/FakeEntity.cs
@@ -0,0 +1,11 @@
+﻿using MK.Domain;
+
+namespace MK.Presistance.Test.Domain
+{
+    /// <summary>
+    /// represent fake entity for testing
+    /// </summary>
+    public class FakeEntity : BaseEntity
+    {
+    }
+}
